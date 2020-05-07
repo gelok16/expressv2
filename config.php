90f0d22218fa541d85a8a474f76d12f7297092ea
gelok16 <64940099+gelok16@users.noreply.github.com>
2020-05-07 10:35:11 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+//Configurado por TUDINEROWEB
+
+// PHPSESSID
+$phpsessid = "5dec8cf98c92d266383264fd6026c80a";
+
+// DOGETOKEN
+$dogetoken = "QPCe6aXS09mDWQJu0PHgB8sBJRpZ6nsQ";
+
+
+
+
+
+
+
+?>
